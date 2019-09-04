b1f2d86711046b1e86b6955f7a7f38a7f6025b98
krishna_verma <kverma@alpha-sense.com>
2019-09-04 11:57:36 +0530

    Adding new learnings related to Thread Concept

---
+++ src/main/java/com/nirvana/learning/threadpool/utils/ExecuterServiceExample.java
@@ -0,0 +1,25 @@
+package com.nirvana.learning.threadpool.utils;
+
+import java.time.LocalTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author KrishnaV
+ */
+public class ExecuterServiceExample {
+    public static void main(String... args) {
+        ScheduledExecutorService timer =
+                Executors.newSingleThreadScheduledExecutor();
+        timer.scheduleAtFixedRate(
+                () -> System.out.println(LocalTime.now()),
+                0, 1 , TimeUnit.SECONDS
+        );
+        timer.scheduleAtFixedRate(
+                () -> System.out.println("Hello World"),
+                0, 200 , TimeUnit.MILLISECONDS
+        );
+        System.out.println("Main Thread: I'm outta here!");
+    }
+}
