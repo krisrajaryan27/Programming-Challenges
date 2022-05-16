bf9ca11d221f52fb2c7df14b8c43b1a70bec85ef
Krishna Verma <krishna.verma@avataar.me>
2022-05-16 22:13:18 +0530

    educative: Adding codes for different algorithms

--- src/main/java/com/nirvana/learning/interview/array/TopKFrequentElements.java
+++ src/main/java/com/nirvana/learning/interview/array/TopKFrequentElements.java
@@ -1,2 +1,48 @@
-package com.nirvana.learning.interview.array;public class TopKFrequentElements {
+package com.nirvana.learning.interview.array;
+
+import java.util.*;
+
+/**
+ * Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+ * <p>
+ * Input: nums = [1,1,1,2,2,3], k = 2
+ * Output: [1,2]
+ * <p>
+ * Input: nums = [1], k = 1
+ * Output: [1]
+ **/
+public class TopKFrequentElements {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        Arrays.stream(topKFrequent(nums, 2)).forEach(System.out::println);
+    }
+
+    public static int[] topKFrequent(int[] nums, int k) {
+        // find the frequency of each number
+        Map<Integer, Integer> numFrequencyMap = new HashMap<>();
+
+        for (int n : nums) {
+            numFrequencyMap.put(n, numFrequencyMap.getOrDefault(n, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((e1, e2) -> e1.getValue() -
+                e2.getValue());
+
+        // go through all numbers of map & push them in a minHeap which will have top k elements
+        // If at any time it has > K elements, then remove the smallest element.
+
+        for (Map.Entry<Integer, Integer> entry : numFrequencyMap.entrySet()) {
+            minHeap.add(entry);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        // Let's create a list of top k elements
+        List<Integer> topNumbers = new ArrayList<>(k);
+        while (!minHeap.isEmpty()) {
+            topNumbers.add(minHeap.poll().getKey());
+        }
+        return topNumbers.stream().mapToInt(i -> i).toArray();
+    }
 }
