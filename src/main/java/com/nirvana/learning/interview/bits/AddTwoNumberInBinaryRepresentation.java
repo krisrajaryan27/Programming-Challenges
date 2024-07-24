d23eea1ea42d4e99ee56e1ce1542a92ca54df89f
krisrajaryan27 <krisv.nit16@gmail.com>
2024-07-24 18:42:02 +0530

    Adding code for logic based problem from codingbat

--- src/main/java/com/nirvana/learning/interview/bits/AddTwoNumberInBinaryRepresentation.java
+++ src/main/java/com/nirvana/learning/interview/bits/AddTwoNumberInBinaryRepresentation.java
@@ -1,88 +1,48 @@
 package com.nirvana.learning.interview.bits;
 
-/**
- * http://www.geeksforgeeks.org/add-two-bit-strings/
- * http://www.geeksforgeeks.org/binary-representation-of-a-given-number/
- * http://www.geeksforgeeks.org/add-two-numbers-without-using-arithmetic-operators/
- */
+
 public class AddTwoNumberInBinaryRepresentation {
 
     public static void main(String[] args) {
-        AddTwoNumberInBinaryRepresentation anp = new AddTwoNumberInBinaryRepresentation();
-        char[] num1 = "1010001110".toCharArray();
-        char[] num2 = "10011".toCharArray();
-        int result = anp.add(num1, num2);
-        System.out.println(anp.addTwoNumbersWithoutArithmeticOperator(296, 5662));
-        System.out.println(anp.addTwoNumbersWithoutArithmeticOperatorFaster(296, 5662));
-        anp.printResult(result);
+        System.out.println(addBinary("1010", "1011"));
     }
 
-    public int add(char[] num1, char[] num2) {
-        int index1 = num1.length - 1;
-        int index2 = num2.length - 1;
+    public static String addBinary(String a, String b) {
+        int lenA = a.length();
+        int lenB = b.length();
+        int maxLength = Math.max(lenA, lenB);
+        char[] result = new char[maxLength + 1]; // +1 for possible carry
+        int i = lenA - 1;
+        int j = lenB - 1;
         int carry = 0;
-        int result = 0;
-        int index = 0;
-        while (index1 >= 0 && index2 >= 0) {
-            int r1 = num1[index1] - '0';
-            int r2 = num2[index2] - '0';
-            result = result | (r1 ^ r2 ^ carry) << index;
-            index1--;
-            index2--;
-            index++;
-            carry = r1 & carry | r2 & carry | r1 & r2;
-        }
-        while (index1 >= 0) {
-            int r1 = num1[index1] - '0';
-            result = result | (r1 ^ carry) << index;
-            index1--;
-            index++;
-            carry = r1 & carry;
-        }
-        while (index2 >= 0) {
-            int r2 = num1[index2] - '0';
-            result = result | (r2 ^ carry) << index;
-            index2--;
-            index++;
-            carry = r2 & carry;
-        }
-        return result;
-    }
+        int index = maxLength;
 
-    public int addTwoNumbersWithoutArithmeticOperator(int num1, int num2) {
-        int carry = 0;
-        int result = 0;
-        for (int i = 0; i <= 31; i++) {
-            int r1 = (num1 & 1 << i) != 0 ? 1 : 0;
-            int r2 = (num2 & 1 << i) != 0 ? 1 : 0;
+        // Loop until both strings are processed and no carry remains
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry; // Start with the carry
 
-            result = result | (r1 ^ r2 ^ carry) << i;
-            if ((r1 & r2) != 0 || (r1 & carry) != 0 || (r2 & carry) != 0) {
-                carry = 1;
-            } else {
-                carry = 0;
+            // Add the last digit of a if available
+            if (i >= 0) {
+                sum += a.charAt(i) - '0'; // Convert char to int
+                i--;
             }
-        }
-        return result;
-    }
 
-    public int addTwoNumbersWithoutArithmeticOperatorFaster(int num1, int num2) {
-        while (num2 != 0) {
-            int carry = num1 & num2;
-            num1 = num1 ^ num2;
-            num2 = carry << 1;
-        }
-        return num1;
-    }
-
-    public void printResult(int num) {
-        for (int i = 1 << 31; i != 0; i = i >>> 1) {
-            if ((num & i) > 0) {
-                System.out.print("1");
-            } else {
-                System.out.print("0");
+            // Add the last digit of b if available
+            if (j >= 0) {
+                sum += b.charAt(j) - '0'; // Convert char to int
+                j--;
             }
+
+            // Store the result (sum mod 2) in the result array
+            result[index] = (char) ((sum % 2) + '0'); // Convert int back to char
+            carry = sum / 2; // Calculate carry for next iteration
+            index--;
         }
+
+        // Convert the result array to a string, skipping leading zeros
+        return new String(result, index + 1, maxLength - index);
     }
+
+
 
 }
