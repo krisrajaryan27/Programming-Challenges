8c5946876b042d17f4db69b808b3bb991a723192
krisrajaryan27 <krisv.nit16@gmail.com>
2024-07-26 21:27:59 +0530

    Updating code for sorting array by parity

--- src/main/java/com/nirvana/learning/interview/array/SortArrayByParity.java
+++ src/main/java/com/nirvana/learning/interview/array/SortArrayByParity.java
@@ -20,15 +20,15 @@ public class SortArrayByParity {
         int leftIndex = 0;
         int rightIndex = nums.length - 1;
         while (leftIndex < rightIndex) {
-            if (nums[leftIndex] % 2 == 1 && nums[rightIndex] % 2 == 0) {
+            if (nums[leftIndex] % 2 == 0) {
+                leftIndex++;
+            } else if (nums[rightIndex] % 2 == 1) {
+                rightIndex--;
+            } else {
                 int temp = nums[leftIndex];
                 nums[leftIndex] = nums[rightIndex];
                 nums[rightIndex] = temp;
-            }
-            if (nums[leftIndex] % 2 == 0) {
                 leftIndex++;
-            }
-            if (nums[rightIndex] % 2 == 1) {
                 rightIndex--;
             }
         }
