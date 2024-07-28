f479c903cb54ab97d3e00e84321c83134a067120
krisrajaryan27 <krisv.nit16@gmail.com>
2024-07-28 14:33:20 +0530

    Adding solution to check if nearby duplicates are present

--- src/main/java/com/nirvana/learning/interview/string/GroupAnagrams.java
+++ src/main/java/com/nirvana/learning/interview/string/GroupAnagrams.java
@@ -3,31 +3,34 @@ package com.nirvana.learning.interview.string;
 import java.util.*;
 
 /**
- * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
- *
+ * Given an array of strings strs, group the anagrams together.
+ * You can return the answer in any order.
+ * <p>
  * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
  * typically using all the original letters exactly once.
- * https://leetcode.com/problems/group-anagrams/
+ * <a href="https://leetcode.com/problems/group-anagrams/">...</a>
  * Example 1:
- *
+ * <p>
  * Input: strs = ["eat","tea","tan","ate","nat","bat"]
  * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
- *
- * */
+ * <p>
+ */
 public class GroupAnagrams {
     public static void main(String[] args) {
-        String[] strs = new String[]{"eat","tea","tan","ate","nat","bat"};
+        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
         System.out.println(groupAnagrams(strs));
+        System.out.println(groupAnagramsM2(strs));
 
     }
+
     //O(m*nlogn)
     public static List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        for(String str: strs){
+        for (String str : strs) {
             char[] ch = str.toCharArray();
             Arrays.sort(ch);
             String key = String.valueOf(ch);
-            if(!map.containsKey(key)){
+            if (!map.containsKey(key)) {
                 map.put(key, new ArrayList<>());
             }
             map.get(key).add(str);
@@ -36,16 +39,16 @@ public class GroupAnagrams {
     }
 
     //O(m*n)
-    public List<List<String>> groupAnagramsM2(String[] strs) {
-        if(strs.length == 0) return new ArrayList<>();
+    public static List<List<String>> groupAnagramsM2(String[] strs) {
+        if (strs.length == 0) return new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
-        for(String str: strs){
+        for (String str : strs) {
             char[] hash = new char[26];
-            for(char ch: str.toCharArray()){
-                hash[ch-'a']++;
+            for (char ch : str.toCharArray()) {
+                hash[ch - 'a']++;
             }
             String key = String.valueOf(hash);
-            if(!map.containsKey(key)){
+            if (!map.containsKey(key)) {
                 map.put(key, new ArrayList<>());
             }
             map.get(key).add(str);
